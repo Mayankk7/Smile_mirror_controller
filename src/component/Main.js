8f2eab573b208abb263f7ed45955f2a6b8d89c43
Mayankk7 <72187440+Mayankk7@users.noreply.github.com>
2022-06-21 18:30:53 +0530

    initial commit

---
+++ src/component/Main.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const Main = () => {
+  const [ip, setip] = useState("");
+
+  const on = async ({ ip }) => {
+    const data = await axios.get(`http://${ip}/relay/on`);
+    console.log(data);
+  };
+
+  const off = async ({ ip }) => {
+    const data = await axios.get(`http://${ip}/relay/off`);
+    console.log(data);
+  };
+
+  const toggle = async ({ ip }) => {
+    const data = await axios.get(`http://${ip}/relay/toggle`);
+    console.log(data);
+  };
+
+  return (
+    <div className='App'>
+      <div className='App-header'>
+        <input
+          type='text'
+          placeholder='Enter the IP'
+          value={ip}
+          onChange={(e) => {
+            setip(e.target.value);
+          }}></input>
+        <button onClick={() => setip(ip)}>Set ip</button>
+      </div>
+      <div className='flex'>
+        <div className='flex-item'>
+          <button className='btn' onClick={() => on({ ip })}>
+            ON
+          </button>
+        </div>
+        <div className='flex-item'>
+          <button
+            className='btn'
+            onClick={() => {
+              off({ ip });
+            }}>
+            OFF
+          </button>
+        </div>
+        <div className='flex-item'>
+          <button
+            className='btn'
+            onClick={() => {
+              toggle({ ip });
+            }}>
+            TOGGLE
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
